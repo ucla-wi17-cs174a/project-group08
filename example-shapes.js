6885d6763135f7cddd86c9c51aefb77f5fab1a99
kenzoneff <kenzoneff@hotmail.com>
2017-02-23 22:58:37 -0800

    kenzo

--- example-shapes.js
+++ example-shapes.js
@@ -24,3 +24,35 @@ Declare_Any_Class( "Tetrahedron",
       }
   }, Shape )
 
+
+  //
+  // Generate fun terrain:
+  //
+  
+  Declare_Any_Class( "Heightmap",
+  { 'populate': function() 
+      {
+        function xzheight(x, z)
+		{
+			var y = -10 + 1*Math.sin(x) + 1*Math.sin(z);
+			return y;
+		}
+		//var img = document.getElementById("heightmap_test.png");
+		
+		
+		dims = [100, 100]
+		
+		for(var i = 0; i < dims[0]; i++)
+			for(var j = 0; j < dims[1]; j++)
+			{
+				this.positions.push(vec3(i - dims[0]/2, xzheight(i,j), j - dims[1]/2));
+				if(i != 0 && j != 0)
+					this.indices.push((j-1 + (i-1)*dims[0]), (j + (i-1)*dims[0]), (j-1 + i*dims[0]), (j + (i-1)*dims[0]), (j-1 + i*dims[0]), (j + i*dims[0]));
+					this.normals.push(vec3(0,1,0), vec3(0,1,0));
+			}
+        
+      }
+  }, Shape )
+  
+  
+  
