31ee97d919d390bb302701efbb12733149110728
Paul Kim <paul.m.kim321@gmail.com>
2017-03-04 21:22:20 -0800

    implemented collection

--- example-shapes.js
+++ example-shapes.js
@@ -24,3 +24,49 @@ Declare_Any_Class( "Tetrahedron",
       }
   }, Shape )
 
+
+// *********** SQUARE ***********
+Declare_Any_Class( "Square",    // A square, demonstrating shared vertices.  On any planar surface, the interior edges don't make any important seams.
+  { 'populate': function()      // In these cases there's no reason not to re-use values of the common vertices between triangles.  This makes all the
+      {                         // vertex arrays (position, normals, etc) smaller and more cache friendly.
+         this.positions     .push( vec3(-1,-1,0), vec3(1,-1,0), vec3(-1,1,0), vec3(1,1,0) ); // Specify the 4 vertices -- the point cloud that our Square needs.
+         this.normals       .push( vec3(0,0,1), vec3(0,0,1), vec3(0,0,1), vec3(0,0,1) );     // ...
+         this.texture_coords.push( vec2(0,0),   vec2(1,0),   vec2(0,1),   vec2(1,1)   );     // ...
+         this.indices       .push( 0, 1, 2,     1, 3, 2 );                                   // Two triangles this time, indexing into four distinct vertices.
+      }
+  }, Shape )
+  
+Declare_Any_Class( "Text_Line", // Draws a rectangle textured with images of ASCII characters textured over each quad, spelling out a string.
+  { 'populate': function( max_size )    // Each quad is a separate rectangle_strip.
+      { this.max_size = max_size;
+        var object_transform = mat4();
+        for( var i = 0; i < max_size; i++ )
+        {
+          Square.prototype.insert_transformed_copy_into( this, [], object_transform );
+          object_transform = mult( object_transform, translation( 1.5, 0, 0 ));
+        }
+      },
+    'draw': function( graphics_state, model_transform, heads_up_display, color )
+      { if( heads_up_display )      { gl.disable( gl.DEPTH_TEST );  }
+        Shape.prototype.draw.call(this, graphics_state, model_transform, new Material( color, 1, 0, 1, 40, "text.png" ) );
+        if( heads_up_display )      { gl.enable(  gl.DEPTH_TEST );  }
+      },
+    'set_string': function( line )
+      { for( var i = 0; i < this.max_size; i++ )
+          {
+            var row = Math.floor( ( i < line.length ? line.charCodeAt( i ) : ' '.charCodeAt() ) / 16 ),
+                col = Math.floor( ( i < line.length ? line.charCodeAt( i ) : ' '.charCodeAt() ) % 16 );
+
+            var skip = 3, size = 32, sizefloor = size - skip;
+            var dim = size * 16,  left  = (col * size + skip) / dim,      top    = (row * size + skip) / dim,
+                                  right = (col * size + sizefloor) / dim, bottom = (row * size + sizefloor + 5) / dim;
+
+            this.texture_coords[ 4 * i ]     = vec2( left,  1 - bottom );
+            this.texture_coords[ 4 * i + 1 ] = vec2( right, 1 - bottom );
+            this.texture_coords[ 4 * i + 2 ] = vec2( left,  1 - top );
+            this.texture_coords[ 4 * i + 3 ] = vec2( right, 1 - top );
+          }
+        gl.bindBuffer( gl.ARRAY_BUFFER, this.graphics_card_buffers[2] );
+        gl.bufferData( gl.ARRAY_BUFFER, flatten(this.texture_coords), gl.STATIC_DRAW );
+      }
+  }, Shape )
