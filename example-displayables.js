e73fc8b0f2632f5e21ac2f2a4352305628992a09
Paul Kim <paul.m.kim321@gmail.com>
2017-03-07 16:16:38 -0800

    cleaned code

--- example-displayables.js
+++ example-displayables.js
@@ -1,6 +1,7 @@
 var RES_RATIO = 16;	
 
-Declare_Any_Class( "Debug_Screen",  // Debug_Screen - An example of a displayable object that our class Canvas_Manager can manage.  Displays a text user interface.
+// Create the textbox
+Declare_Any_Class( "Debug_Screen",
   { 'construct': function( context )
       { this.define_data_members( { shared_scratchpad: context.shared_scratchpad, numCollected: 0, graphicsState: new Graphics_State() } );
         shapes_in_use.debug_text = new Text_Line( 35 );
@@ -9,7 +10,8 @@ Declare_Any_Class( "Debug_Screen",  // Debug_Screen - An example of a displayabl
     'init_keys': function( controls )
       { 
       },
-    'update_strings': function( debug_screen_object )   // Strings that this displayable object (Debug_Screen) contributes to the UI:
+	  // update the values in string
+    'update_strings': function( debug_screen_object )
       { 
 		this.numCollected = this.shared_scratchpad.numCollected;
       },
@@ -22,18 +24,17 @@ Declare_Any_Class( "Debug_Screen",  // Debug_Screen - An example of a displayabl
             model_transform = mult( translation( -.95, -.9, 0 ), font_scale );
 
 	
+		// draw the text box
 		shapes_in_use.debug_text.set_string("Collected: " + this.numCollected.toString() );
 		shapes_in_use.debug_text.draw( this.graphicsState, model_transform, true, vec4(0,0,0,1) );  // Draw some UI text (strings)
 		model_transform = mult( translation( 0, .08, 0 ), model_transform );
-	
-
       }
   }, Animation );
 
-
+// initial camera creation
 Declare_Any_Class("Example_Camera", {
-    'construct': function(context) { // 1st parameter below is our starting camera matrix.  2nd is the projection:  The matrix that determines how depth is treated.  It projects 3D points onto a plane.
-        context.shared_scratchpad.graphics_state = new Graphics_State(translation(0, -1, -50), perspective(45, canvas.width / canvas.height, .1, 1000), 0);
+    'construct': function(context) {
+        context.shared_scratchpad.graphics_state = new Graphics_State(translation(0, -1, -10), perspective(45, canvas.width / canvas.height, .1, 1000), 0);
         this.define_data_members({
             graphics_state: context.shared_scratchpad.graphics_state,
             thrust: vec3(),
@@ -47,20 +48,25 @@ Declare_Any_Class("Example_Camera", {
 
 }, Animation);
 
+// main animation program
 Declare_Any_Class("Example_Animation", {
     'construct': function(context) {
         this.shared_scratchpad = context.shared_scratchpad;
 
-        shapes_in_use.tetrahedron = new Tetrahedron();
-        shapes_in_use.sphere = new Subdivision_Sphere(4);
-		shapes_in_use.collection_object1 = new Collection_Object(4, 50, 0, -150);
-		shapes_in_use.collection_object2 = new Collection_Object(4, -50, 0, -150);
-		shapes_in_use.collection_object3 = new Collection_Object(4, 50, 0, -100);
-		shapes_in_use.collection_object4 = new Collection_Object(4, -50, 0, -100);
-		
-		shapes_in_use.imported = Imported_Object.prototype.auto_flat_shaded_version();;
-		shapes_in_use.square = new Square();
-		
+		// declare all variables
+		// creating collection objects
+		shapes_in_use.collection_object = new Array();
+		// declare any number of objects
+		shapes_in_use.collection_object.push(new Collection_Object(4, 50, 0, -150));
+		shapes_in_use.collection_object.push(new Collection_Object(4, -50, 0, -150));
+		shapes_in_use.collection_object.push(new Collection_Object(4, 50, 0, -100));
+		shapes_in_use.collection_object.push(new Collection_Object(4, -50, 0, -100));
+
+		// create imported plane
+		shapes_in_use.plane = Imported_Object.prototype.auto_flat_shaded_version();;
+
+
+		// declare heightmap variables
 		shapes_in_use.heightmap = new Heightmap;
 		shapes_in_use.terrain1 = new Terrain(vec3(0, -32, -32), 32);
 		shapes_in_use.terrain2 = new Terrain(vec3(0, -32, -64), 32);
@@ -82,8 +88,11 @@ Declare_Any_Class("Example_Animation", {
 			//After geometry is made, add it to the list of stuff to draw, and purge the lower resolution geometry that is replaced
 		var to_draw = []	//All the geometry in here is what gets drawn
 		
+		
+		// declare heading, pitch, xyz, and speed of the plane
         this.shared_scratchpad.heading = 0;
         this.shared_scratchpad.pitch = 0;
+		this.shared_scratchpad.roll = 0;
 
         this.shared_scratchpad.x = 0;
         this.shared_scratchpad.y = 0;
@@ -91,12 +100,8 @@ Declare_Any_Class("Example_Animation", {
         this.shared_scratchpad.speed = 0.1;
 		
 		this.shared_scratchpad.speed_change = 0; // 0: no change; -1: slow down; +1: speed up;
-		this.shared_scratchpad.right = 0;
-		this.shared_scratchpad.left = 0;
-		this.shared_scratchpad.up = 0;
-		this.shared_scratchpad.down = 0;
-		this.shared_scratchpad.pitch_change = 0;
-		this.shared_scratchpad.heading_change = 0;
+		this.shared_scratchpad.pitch_change = 0; // how much to change pitch
+		this.shared_scratchpad.heading_change = 0; // how much to change heading
     },
     'init_keys': function(controls) {
         controls.add("up", this, function() {
@@ -265,8 +270,26 @@ Declare_Any_Class("Example_Animation", {
 		controls.add( ".", this, function() { 
 			this.shared_scratchpad.speed_change =  0; }, {'type':'keyup'} 
 		);
+		
+		// reset
+		controls.add("ctrl+r", this, function() {
+			this.shared_scratchpad.heading = 0;
+			this.shared_scratchpad.pitch = 0;
+			this.shared_scratchpad.roll = 0;
+
+			this.shared_scratchpad.x = 0;
+			this.shared_scratchpad.y = 0;
+			this.shared_scratchpad.z = 0;
+			this.shared_scratchpad.speed = 0.1;
+			
+			this.shared_scratchpad.speed_change = 0; // 0: no change; -1: slow down; +1: speed up;
+			this.shared_scratchpad.pitch_change = 0; // how much to change pitch
+			this.shared_scratchpad.heading_change = 0; // how much to change heading
+
+		});
 
     },
+	// check collision between two spheres
 	'checkCollision' : function(x1, y1, z1, r1, x2, y2, z2, r2) {
 		// Exit if separated along an axis
 		if ( (x1+r1) < (x2-r2) || (x1-r1) > (x2+r2) ) return false;
@@ -294,132 +317,110 @@ Declare_Any_Class("Example_Animation", {
         graphics_state.lights.push(new Light(vec4(-10, 10, 0, 1), Color(1, 0, 0, 1), 100000));
         // *** Materials: *** Declare new ones as temps when needed; they're just cheap wrappers for some numbers.
         // 1st parameter:  Color (4 floats in RGBA format), 2nd: Ambient light, 3rd: Diffuse reflectivity, 4th: Specular reflectivity, 5th: Smoothness exponent, 6th: Texture image.
-        var sphereMaterial = new Material(Color(1, 0, 1, 1), .4, .4, .8, 40); // Omit the final (string) parameter if you want no texture
+        var collectableMaterial = new Material(Color(1, 0, 1, 1), .4, .4, .8, 40); // Omit the final (string) parameter if you want no texture
         var tetraMaterial = new Material(Color(0, 1, 1, 1), .4, .4, .4, 40); // Omit the final (string) parameter if you want no texture
 		var landMaterial = new Material(Color(0.4, 0.5, 0, 1), .6, .8, .4, 4);	//Just a placeholder for now
-
-
 
 		// Draw map
         model_transform = mult(model_transform, translation(0, 0, -100));;
 		shapes_in_use.terrain1.draw(graphics_state, model_transform, landMaterial);
         model_transform = mult(model_transform, translation(0, 0, 100));
 
+		
 		// DRAW PLANE This is rather verbose and should get fixed
         // create tetrahedron for temp plane
 		// modify speed based on key input
 		var speed_change = 0.01;
-		if(this.shared_scratchpad.speed_change < 0) // slowing down
+		if(this.shared_scratchpad.speed_change < 0 && this.shared_scratchpad.speed > 0) // slowing down. Min speed is 0
 		{
-			if(this.shared_scratchpad.speed > 0)
-			{
-				this.shared_scratchpad.speed -= speed_change;
-				if(this.shared_scratchpad.speed < 0)
-					this.shared_scratchpad.speed = 0;
-			}
+			this.shared_scratchpad.speed -= speed_change;
+			if(this.shared_scratchpad.speed < 0)
+				this.shared_scratchpad.speed = 0;
 		}
-		else if(this.shared_scratchpad.speed_change > 0) // speeding up
+		else if(this.shared_scratchpad.speed_change > 0 && this.shared_scratchpad.speed < 1) // speeding up. Max speed is 1
 		{
-			if(this.shared_scratchpad.speed < 1)
-			{
-				this.shared_scratchpad.speed += speed_change;
-			}
+			this.shared_scratchpad.speed += speed_change;
 		}
+		
 		// modify heading and pitch based on key input
-
-		this.shared_scratchpad.pitch += this.shared_scratchpad.pitch_change;
-
+		if(this.shared_scratchpad.pitch <= 90 && this.shared_scratchpad.pitch >= -90)
+		{
+			this.shared_scratchpad.pitch += this.shared_scratchpad.pitch_change;
+			if(this.shared_scratchpad.pitch > 90)
+			{
+				this.shared_scratchpad.pitch = 90;
+			}
+			else if(this.shared_scratchpad.pitch < -90)
+			{
+				this.shared_scratchpad.pitch = -90;
+			}
+		}
 		this.shared_scratchpad.heading += this.shared_scratchpad.heading_change;
+		if(this.shared_scratchpad.heading > 360)
+			this.shared_scratchpad.heading -= 360;
+		else if(this.shared_scratchpad.heading < 360)
+			this.shared_scratchpad.heading += 360;
 		
-		
-        // move forward based on current heading
+        // move forward based on current pitch and heading
         var forward_speed = this.shared_scratchpad.speed;
+		var pitch = this.shared_scratchpad.pitch;
+		var heading = this.shared_scratchpad.heading;
 
+		var pitch_x = Math.round(Math.cos(radians(heading))*100)/100;
+		var pitch_z = -1 * Math.round(Math.sin(radians(heading))*100)/100;
+
+		
+		
         var y_change = Math.sin(radians(this.shared_scratchpad.pitch)) * forward_speed;
         var xz_change = Math.cos(radians(this.shared_scratchpad.pitch)) * forward_speed;
-
         var x_change = -1 * Math.sin(radians(this.shared_scratchpad.heading)) * xz_change;
         var z_change = -1 * Math.cos(radians(this.shared_scratchpad.heading)) * xz_change;
-
+				
+		// add to the current position
         this.shared_scratchpad.x += x_change;
         this.shared_scratchpad.y += y_change;
         this.shared_scratchpad.z += z_change;
+		
+		// calculate roll
+		if(this.shared_scratchpad.roll < this.shared_scratchpad.heading_change * 50)
+		{
+			this.shared_scratchpad.roll += 1;
+		}
+		else if(this.shared_scratchpad.roll > this.shared_scratchpad.heading_change * 50)
+		{
+			this.shared_scratchpad.roll -= 1;
+		}
+
 
 		// draw plane
-        model_transform = mult(model_transform, translation(this.shared_scratchpad.x, this.shared_scratchpad.y, this.shared_scratchpad.z));
-        model_transform = mult(model_transform, rotation(this.shared_scratchpad.heading, 0, 1, 0));
-        model_transform = mult(model_transform, rotation(this.shared_scratchpad.pitch, 1, 0, 0));
-        shapes_in_use.tetrahedron.draw(graphics_state, model_transform, tetraMaterial);
+		model_transform = mult(model_transform, translation(this.shared_scratchpad.x, this.shared_scratchpad.y, this.shared_scratchpad.z)); //position
+		model_transform = mult(model_transform, rotation(this.shared_scratchpad.pitch, pitch_x, 0, pitch_z));
+		model_transform = mult(model_transform, rotation(this.shared_scratchpad.heading, 0, 1, 0));
+		model_transform = mult(model_transform, rotation(this.shared_scratchpad.roll, 0, 0, 1));
+        model_transform = mult(model_transform, rotation(90, 0, 1, 0)); // current model is 90 degrees off
+        shapes_in_use.plane.draw(graphics_state, model_transform, tetraMaterial);
 
 
 		// DRAW COLLECTION_OBJECT
-		// create collection objects and check if it exists. Solid programming style right here
-		var cur_collection = shapes_in_use.collection_object1;
-		if(cur_collection.collected == false)
+		// create collection objects and check if it exists
+		for(var i = 0; i < shapes_in_use.collection_object.length; i++)
 		{
-			if(this.checkCollision(this.shared_scratchpad.x, this.shared_scratchpad.y, this.shared_scratchpad.z, 1, cur_collection.x, cur_collection.y, cur_collection.z, 1))
+			var cur_collection = shapes_in_use.collection_object[i];
+			if(cur_collection.collected == false)
 			{
-				cur_collection.collected = true;
-				this.shared_scratchpad.numCollected += 1;
-			}
-			else
-			{
-				model_transform = mat4();
-				model_transform = mult(model_transform, translation(cur_collection.x, cur_collection.y, cur_collection.z));
-				cur_collection.draw(graphics_state, model_transform, sphereMaterial);
+				if(this.checkCollision(this.shared_scratchpad.x, this.shared_scratchpad.y, this.shared_scratchpad.z, 1, cur_collection.x, cur_collection.y, cur_collection.z, 1))
+				{
+					cur_collection.collected = true;
+					this.shared_scratchpad.numCollected += 1;
+				}
+				else
+				{
+					model_transform = mat4();
+					model_transform = mult(model_transform, translation(cur_collection.x, cur_collection.y, cur_collection.z));
+					cur_collection.draw(graphics_state, model_transform, collectableMaterial);
+				}
 			}
 		}
-		var cur_collection = shapes_in_use.collection_object2;
-		if(cur_collection.collected == false)
-		{
-			if(this.checkCollision(this.shared_scratchpad.x, this.shared_scratchpad.y, this.shared_scratchpad.z, 1, cur_collection.x, cur_collection.y, cur_collection.z, 1))
-			{
-				cur_collection.collected = true;
-				this.shared_scratchpad.numCollected += 1;
-
-			}
-			else
-			{
-				model_transform = mat4();
-				model_transform = mult(model_transform, translation(cur_collection.x, cur_collection.y, cur_collection.z));
-				cur_collection.draw(graphics_state, model_transform, sphereMaterial);
-			}
-		}
-		var cur_collection = shapes_in_use.collection_object3;
-		if(cur_collection.collected == false)
-		{
-			if(this.checkCollision(this.shared_scratchpad.x, this.shared_scratchpad.y, this.shared_scratchpad.z, 1, cur_collection.x, cur_collection.y, cur_collection.z, 1))
-			{
-				cur_collection.collected = true;
-				this.shared_scratchpad.numCollected += 1;
-
-			}
-			else
-			{
-				model_transform = mat4();
-				model_transform = mult(model_transform, translation(cur_collection.x, cur_collection.y, cur_collection.z));
-				cur_collection.draw(graphics_state, model_transform, sphereMaterial);
-			}
-		}
-		var cur_collection = shapes_in_use.collection_object4;
-		if(cur_collection.collected == false)
-		{
-			if(this.checkCollision(this.shared_scratchpad.x, this.shared_scratchpad.y, this.shared_scratchpad.z, 1, cur_collection.x, cur_collection.y, cur_collection.z, 1))
-			{
-				cur_collection.collected = true;
-				this.shared_scratchpad.numCollected += 1;
-
-			}
-			else
-			{
-				model_transform = mat4();
-				model_transform = mult(model_transform, translation(cur_collection.x, cur_collection.y, cur_collection.z));
-				model_transform = mult(model_transform, scale(5,5,5));
-				shapes_in_use.imported.draw(graphics_state, model_transform, sphereMaterial);
-			}
-		}
-
-		
 		
 		// make camera follow the plane
         this.shared_scratchpad.graphics_state.camera_transform = mat4();
