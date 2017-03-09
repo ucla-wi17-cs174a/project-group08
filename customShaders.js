aa9fb06bc4e4ebd4ef9f0ebfcab3c09b65627f19
Iyal Suresh <Iyal Suresh>
2017-03-08 17:18:20 -0800

    Not sure what all I've accomplished tbh. Lots of trial, even more errors

--- customShaders.js
+++ customShaders.js
@@ -1,3 +1,156 @@
+Declare_Any_Class( "G_buf_gen_NormalSpray_hf",
+  { 'update_uniforms'          : function( g_state, model_transform, material )     // Send javascrpt's variables to the GPU to update its overall state.
+      {
+		
+          let [ P, C, M ]  = [ g_state.projection_transform, g_state.camera_transform, model_transform ],   // PCM will mean Projection * Camera * Model
+          CM             = mult( C,  M ),
+          PCM            = mult( P, CM ),                               // Send the current matrices to the shader.  Go ahead and pre-compute the products
+          inv_trans_CM   = toMat3( transpose( inverse( CM ) ) );        // we'll need of the of the three special matrices and just send those, since these
+                                                                        // will be the same throughout this draw call & across each instance of the vertex shader.
+        gl.uniformMatrix4fv( g_addrs.camera_transform_loc,                  false, flatten(  C  ) );
+        gl.uniformMatrix4fv( g_addrs.camera_model_transform_loc,            false, flatten(  CM ) );
+        gl.uniformMatrix4fv( g_addrs.projection_camera_model_transform_loc, false, flatten( PCM ) );
+        gl.uniformMatrix3fv( g_addrs.camera_model_transform_normal_loc,     false, flatten( inv_trans_CM ) );
+
+      },
+    'vertex_glsl_code_string'  : function()           // ********* VERTEX SHADER *********
+      { return `
+	  
+		
+		
+        attribute vec3 vPosition, vNormal;
+        attribute vec2 vTexCoord;
+        varying vec2 fTexCoord;
+		varying vec3 fNormal;
+		varying vec4 pos;
+		
+		uniform mat4 projection_camera_model_transform;
+		uniform mat4 camera_model_transform;
+		
+		void main()
+		{
+			
+            vec4 ospos = vec4(vPosition, 1.0);
+            gl_Position = projection_camera_model_transform * ospos;
+            fTexCoord = vTexCoord;
+			fNormal = vNormal;
+			pos = ( camera_model_transform * ospos );
+		}
+	  `;
+      },
+    'fragment_glsl_code_string': function()           // ********* FRAGMENT SHADER *********
+      { return `
+	  #extension GL_EXT_draw_buffers : require
+		
+		precision mediump float;
+		
+		// const float x = 1.0/255.0;
+		// const float y = 1.0/65025.0;
+		// const float z = 1.0/16581375.0;
+		
+		// vec4 EncodeFloatRGBA( float v ) {
+		  // vec4 enc = vec4(1.0, 255.0, 65025.0, 16581375.0) * v;
+		  // enc = fract(enc);
+		  // enc -= enc.yzww * vec4(x,x,x,0.0);
+		  // return enc;
+		// }
+		// float DecodeFloatRGBA( vec4 rgba ) {
+		  // return dot( rgba, vec4(1.0, x, y, z) );
+		// }
+	  
+		
+	
+		varying vec3 fNormal;
+		varying vec2 fTexCoord;
+		varying vec4 pos;
+		
+		uniform sampler2D texture;
+		uniform bool USE_TEXTURE;
+		
+		void main()
+		{
+		float x_com = abs(dot(fNormal,vec3(1,0,0)));
+		float y_com = abs(dot(fNormal,vec3(0,1,0)));
+		float z_com = abs(dot(fNormal,vec3(0,0,1)));
+		//gl_FragColor = vec4(x_com,y_com,z_com,1.0);
+		//gl_FragData[0] = vec4(pos.x,pos.y,pos.z,1.0);
+		gl_FragData[0] = vec4(x_com,y_com,z_com,1.0);
+		vec3 biasedNorm = (fNormal+1.0)*.5;
+		gl_FragData[1] = vec4(biasedNorm,1.0);
+		gl_FragData[2] = pos;
+		// gl_FragData[0] = vec4(.25,.25,.25,1.0);
+		// gl_FragData[1] = vec4(.5,.5,.5,1.0);
+		// gl_FragData[2] = vec4(.75,.75,.75,1.0);
+		gl_FragData[3] = vec4(1.0,1.0,1.0,1.0);
+		
+		
+		
+		// if(USE_TEXTURE)
+			// gl_FragData[0] = texture2D(texture,fTexCoord);
+		}
+	  `;
+      }
+  }, Shader );
+  
+  Declare_Any_Class( "G_buf_light_NormalSpray_hf",
+  { 'update_uniforms'          : function( g_state, model_transform, material )     // Send javascrpt's variables to the GPU to update its overall state.
+      {
+		
+		gl.uniform1i(g_addrs.tex1_loc, 0);
+		gl.uniform1i(g_addrs.tex2_loc, 1);
+		gl.uniform1i(g_addrs.tex3_loc, 2);
+		gl.uniform1i(g_addrs.tex4_loc, 3);
+      },
+    'vertex_glsl_code_string'  : function()           // ********* VERTEX SHADER *********
+      { return `
+	  
+		
+		
+        attribute vec3 vPosition;
+        attribute vec2 vTexCoord;
+        varying vec2 fTexCoord;
+		
+		void main()
+		{
+			
+            gl_Position = vec4(vPosition, 1.0);
+            fTexCoord = vTexCoord;
+		}
+	  `;
+      },
+    'fragment_glsl_code_string': function()           // ********* FRAGMENT SHADER *********
+      { return `
+		precision mediump float;
+	
+		varying vec2 fTexCoord;
+		
+		uniform sampler2D tex1;
+		uniform sampler2D tex2;
+		uniform sampler2D tex3;
+		uniform sampler2D tex4;
+		
+		void main()
+		{
+			vec3 fNormal = texture2D(tex2,fTexCoord).xyz;
+			fNormal = 2.0*fNormal-1.0;
+			// vec4 p = texture2D(tex1,fTexCoord);
+			float x_com = abs(dot(fNormal,vec3(1,0,0)));
+			float y_com = abs(dot(fNormal,vec3(0,1,0)));
+			float z_com = abs(dot(fNormal,vec3(0,0,1)));
+			// vec4 err = vec4(x_com,y_com,z_com,0.0);
+			// err = p-err;
+			//gl_FragColor = vec4(p.x,p.y,p.z,1.0);
+			gl_FragColor = vec4(x_com,y_com,z_com,1.0);
+			//vec4 pos = texture2D(tex3,fTexCoord);
+			//gl_FragColor = vec4(abs(pos.x),abs(pos.y),abs(pos.z),1.0);
+			//gl_FragColor = vec4(1.0,0.0,1.0,1.0);
+			//gl_FragColor = err;
+		}
+	  `;
+      }
+  }, Shader );
+  
+
 Declare_Any_Class( "G_buf_gen_NormalSpray",
   { 'update_uniforms'          : function( g_state, model_transform, material )     // Send javascrpt's variables to the GPU to update its overall state.
       {
