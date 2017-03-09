7159670aa5a00c6639f629949229dfbf9cd7e26d
Paul Kim <paul.m.kim321@gmail.com>
2017-03-09 00:48:44 -0800

    fixed movement

--- example-displayables.js
+++ example-displayables.js
@@ -66,7 +66,8 @@ Declare_Any_Class("Example_Animation", {
 		shapes_in_use.collection_object.push(new Collection_Object(4, -50, 0, -100));
 
 		// create imported plane
-		shapes_in_use.plane = Imported_Object.prototype.auto_flat_shaded_version();;
+		shapes_in_use.plane = Imported_Object.prototype.auto_flat_shaded_version();
+
 		
 		shapes_in_use.square = new Square();
 		shapes_in_use.skybox = new Cube();
@@ -81,22 +82,24 @@ Declare_Any_Class("Example_Animation", {
 		shapes_in_use.terrain.populate_GPU(vec3(32,-32,0), 32, this.shared_scratchpad.graphics_state);
 		
 		
-		
-		
-		// declare heading, pitch, xyz, and speed of the plane
-        this.shared_scratchpad.heading = 0;
-        this.shared_scratchpad.pitch = 0;
-		this.shared_scratchpad.roll = 0;
-		this.shared_scratchpad.dirVec = vec4(0,0,-1,1.0);
 
         this.shared_scratchpad.x = 0;
         this.shared_scratchpad.y = 0;
         this.shared_scratchpad.z = 0;
         this.shared_scratchpad.speed = 0.1;
 		
+		this.shared_scratchpad.heading = 0;
+		this.shared_scratchpad.pitch = 0;
 		this.shared_scratchpad.speed_change = 0; // 0: no change; -1: slow down; +1: speed up;
 		this.shared_scratchpad.pitch_change = 0; // how much to change pitch
 		this.shared_scratchpad.heading_change = 0; // how much to change heading
+		this.shared_scratchpad.roll_change = 0;
+		
+		this.shared_scratchpad.orientation = mat4(1); // create identity matrix as orientation
+		this.shared_scratchpad.position = vec3(0,0,0);
+		
+		this.shared_scratchpad.camera_extra_pitch = 0;
+		this.shared_scratchpad.camera_extra_heading = 0;
     },
     'init_keys': function(controls) {
         controls.add("up", this, function() {
@@ -248,6 +251,32 @@ Declare_Any_Class("Example_Animation", {
 			this.shared_scratchpad.pitch_change =  0; }, {'type':'keyup'} 
 		);
 
+		// roll left and right: set to 'f'-'g': Left; 'h'-'j': right
+        controls.add("f", this, function() {
+            this.shared_scratchpad.roll_change = -1;
+        });
+		controls.add( "f", this, function() { 
+			this.shared_scratchpad.roll_change =  0; }, {'type':'keyup'} 
+		);
+		controls.add("g", this, function() {
+            this.shared_scratchpad.roll_change = -0.5;
+        });
+		controls.add( "g", this, function() { 
+			this.shared_scratchpad.roll_change =  0; }, {'type':'keyup'} 
+		);
+		controls.add("h", this, function() {
+            this.shared_scratchpad.roll_change = 1;
+        });
+		controls.add( "h", this, function() { 
+			this.shared_scratchpad.roll_change =  0; }, {'type':'keyup'} 
+		);
+		controls.add("j", this, function() {
+            this.shared_scratchpad.roll_change = 0.5;
+        });
+		controls.add( "j", this, function() { 
+			this.shared_scratchpad.roll_change =  0; }, {'type':'keyup'} 
+		);
+		
         // slow down
         controls.add(",", this, function() {
 			this.shared_scratchpad.speed_change = -SPEED_INC;
@@ -284,6 +313,7 @@ Declare_Any_Class("Example_Animation", {
 		});
 
     },
+	
 	// check collision between two spheres
 	'checkCollision' : function(x1, y1, z1, r1, x2, y2, z2, r2) {
 		// Exit if separated along an axis
@@ -293,7 +323,8 @@ Declare_Any_Class("Example_Animation", {
 		// Overlapping on all axes means there is an intersection
 		return true;// Exit if separated along an axis
 	},
-    'display': function(time) {
+    
+	'display': function(time) {
 		
 		var aMaterial = new Material(Color(0.4, 0.5, 0, 1), .6, .8, .4, 4,"FAKE.CHICKEN");	//Just a placeholder for now
 		var skyMat = new Material(Color(1.0,1.0,1.0,1.0), 1.0, 1.0, 0.0, 0.0, "LameBox.png");
@@ -342,91 +373,16 @@ Declare_Any_Class("Example_Animation", {
         var tetraMaterial = new Material(Color(0, 1, 1, 1), .4, .4, .4, 40); // Omit the final (string) parameter if you want no texture
 		var landMaterial = new Material(Color(0.4, 0.5, 0, 1), .6, .8, .4, 4);	//Just a placeholder for now
 
-
-
-		
-		// DRAW PLANE This is rather verbose and should get fixed
-        // create tetrahedron for temp plane
-		// modify speed based on key input
-		// var speed_change = 0.01;
-		// if(this.shared_scratchpad.speed_change < 0 && this.shared_scratchpad.speed > 0) // slowing down. Min speed is 0
-		// {
-			// this.shared_scratchpad.speed -= speed_change;
-			// if(this.shared_scratchpad.speed < 0)
-				// this.shared_scratchpad.speed = 0;
-		// }
-		// else if(this.shared_scratchpad.speed_change > 0 && this.shared_scratchpad.speed < 1) // speeding up. Max speed is 1
-		// {
-			// this.shared_scratchpad.speed += speed_change;
-		// }
-		this.shared_scratchpad.speed = Math.min(1,Math.max(0,this.shared_scratchpad.speed + this.shared_scratchpad.speed_change));
-		// modify heading and pitch based on key input
-		if(this.shared_scratchpad.pitch <= 90 && this.shared_scratchpad.pitch >= -90)
-		{
-			this.shared_scratchpad.pitch += this.shared_scratchpad.pitch_change;
-			if(this.shared_scratchpad.pitch > 90)
-			{
-				this.shared_scratchpad.pitch = 90;
-			}
-			else if(this.shared_scratchpad.pitch < -90)
-			{
-				this.shared_scratchpad.pitch = -90;
-			}
-		}
-		this.shared_scratchpad.heading += this.shared_scratchpad.heading_change;
-		if(this.shared_scratchpad.heading > 360)
-			this.shared_scratchpad.heading -= 360;
-		else if(this.shared_scratchpad.heading < 360)
-			this.shared_scratchpad.heading += 360;
-		
-        // move forward based on current pitch and heading
-        var forward_speed = this.shared_scratchpad.speed;
-		var pitch = this.shared_scratchpad.pitch;
-		var heading = this.shared_scratchpad.heading;
-
-		var pitch_x = Math.round(Math.cos(radians(heading))*100)/100;
-		var pitch_z = -1 * Math.round(Math.sin(radians(heading))*100)/100;
-
-		
-		
-        var y_change = Math.sin(radians(this.shared_scratchpad.pitch)) * forward_speed;
-        var xz_change = Math.cos(radians(this.shared_scratchpad.pitch)) * forward_speed;
-        var x_change = -1 * Math.sin(radians(this.shared_scratchpad.heading)) * xz_change;
-        var z_change = -1 * Math.cos(radians(this.shared_scratchpad.heading)) * xz_change;
-				
-		// add to the current position
-        this.shared_scratchpad.x += x_change;
-        this.shared_scratchpad.y += y_change;
-        this.shared_scratchpad.z += z_change;
-		
-		// calculate roll
-		if(this.shared_scratchpad.roll < this.shared_scratchpad.heading_change * 50)
-		{
-			this.shared_scratchpad.roll += 1;
-		}
-		else if(this.shared_scratchpad.roll > this.shared_scratchpad.heading_change * 50)
-		{
-			this.shared_scratchpad.roll -= 1;
-		}
-
-
+		var current_orientation = this.shared_scratchpad.orientation;
 		// draw plane
-		model_transform = mult(model_transform, translation(this.shared_scratchpad.x, this.shared_scratchpad.y, this.shared_scratchpad.z)); //position
-		model_transform = mult(model_transform, rotation(this.shared_scratchpad.pitch, pitch_x, 0, pitch_z));
-		model_transform = mult(model_transform, rotation(this.shared_scratchpad.heading, 0, 1, 0));
-		model_transform = mult(model_transform, rotation(this.shared_scratchpad.roll, 0, 0, 1));
-        model_transform = mult(model_transform, rotation(90, 0, 1, 0)); // current model is 90 degrees off
-        shapes_in_use.plane.draw(graphics_state, model_transform, tetraMaterial);
+		var planeLocation = this.drawPlane(graphics_state, tetraMaterial);
 
+		// draw collectable
 		this.drawCollectables(graphics_state, collectableMaterial); //HACK FIX. <- make collectables a class and/or interface for object oriented happiness :D
 
 		// make camera follow the plane
-        this.shared_scratchpad.graphics_state.camera_transform = mat4();
-        this.shared_scratchpad.graphics_state.camera_transform = mult(this.shared_scratchpad.graphics_state.camera_transform, rotation(10, 1, 0, 0));
-        this.shared_scratchpad.graphics_state.camera_transform = mult(this.shared_scratchpad.graphics_state.camera_transform, translation(0, -5, -10));
-        this.shared_scratchpad.graphics_state.camera_transform = mult(this.shared_scratchpad.graphics_state.camera_transform, rotation(this.shared_scratchpad.heading, 0, -1, 0));
-        this.shared_scratchpad.graphics_state.camera_transform = mult(this.shared_scratchpad.graphics_state.camera_transform, rotation(this.shared_scratchpad.pitch, -1, 0, 0));
-        this.shared_scratchpad.graphics_state.camera_transform = mult(this.shared_scratchpad.graphics_state.camera_transform, translation(-1 * this.shared_scratchpad.x, -1 * this.shared_scratchpad.y, -1 * this.shared_scratchpad.z));
+		this.drawCamera(graphics_state, current_orientation);
+	
 	
 		//Hacky skyboxes, do properly later
 		this.sbtrans = new mat4();
@@ -436,6 +392,7 @@ Declare_Any_Class("Example_Animation", {
 		invRot = mult(rotation(this.shared_scratchpad.pitch, -1, 0, 0),invRot);
 		this.sbtrans = mult(inverse(this.shared_scratchpad.graphics_state.camera_transform),invRot);
 	},
+	
 	'drawCollectables': function(graphics_state, collectableMaterial){
 		// DRAW COLLECTION_OBJECT
 		// create collection objects and check if it exists
@@ -444,7 +401,7 @@ Declare_Any_Class("Example_Animation", {
 			var cur_collection = shapes_in_use.collection_object[i];
 			if(cur_collection.collected == false)
 			{
-				if(this.checkCollision(this.shared_scratchpad.x, this.shared_scratchpad.y, this.shared_scratchpad.z, 1, cur_collection.x, cur_collection.y, cur_collection.z, 1))
+				if(this.checkCollision(this.shared_scratchpad.position[0], this.shared_scratchpad.position[1], this.shared_scratchpad.position[2], 1, cur_collection.x, cur_collection.y, cur_collection.z, 1))
 				{
 					cur_collection.collected = true;
 					this.shared_scratchpad.numCollected += 1;
@@ -457,5 +414,89 @@ Declare_Any_Class("Example_Animation", {
 				}
 			}
 		}
+	},
+	'drawPlane': function(graphics_state, material){
+		// draw plane
+				
+		// change speed
+		this.shared_scratchpad.speed = Math.min(1,Math.max(0,this.shared_scratchpad.speed + this.shared_scratchpad.speed_change));
+		
+		var orientation = this.shared_scratchpad.orientation;
+		var pitch = new vec3(orientation[0][0], orientation[1][0], orientation[2][0]); // right
+		pitch = mult_vec_scalar(pitch, this.shared_scratchpad.pitch_change);
+		var yaw = new vec3(orientation[0][1], orientation[1][1], orientation[2][1]); // up
+		yaw = mult_vec_scalar(yaw, this.shared_scratchpad.heading_change);
+		var roll = new vec3(-1*orientation[0][2], -1*orientation[1][2], -1*orientation[2][2]); //forward
+		var direction = roll;
+		roll = mult_vec_scalar(roll, this.shared_scratchpad.roll_change);
+		
+		var orientationChange = add(add(pitch, yaw), roll);
+		var angularChange = magnitude(orientationChange); // scalar
+		
+		var overallChange = mat4(1);
+		if(angularChange != 0) {
+			var rotationAxis = normalize(orientationChange); // vector
+			overallChange = rotation(angularChange, rotationAxis);
+			
+			this.shared_scratchpad.orientation = mult( overallChange, this.shared_scratchpad.orientation);
+		}
+		
+		this.shared_scratchpad.position = add(this.shared_scratchpad.position, mult_vec_scalar(normalize(direction), this.shared_scratchpad.speed));
+		
+		var transition = new mat4();
+		transition = mult(transition, translation(this.shared_scratchpad.position[0], this.shared_scratchpad.position[1], this.shared_scratchpad.position[2]));
+		transition = mult(transition, this.shared_scratchpad.orientation)
+		
+		shapes_in_use.plane.draw(graphics_state, transition, material);
+		
+		return transition;
+		
+	},
+	'drawCamera': function(graphics_state, current_orientation){
+		// get pitch, yaw, and roll of plane. If heading or pitch is changing, exaggerage camera
+		var max_change = 20;
+	
+		var orientation = current_orientation;
+
+		if(this.shared_scratchpad.pitch_change > 0 && this.shared_scratchpad.camera_extra_pitch < max_change)
+			this.shared_scratchpad.camera_extra_pitch += 1;
+		else if(this.shared_scratchpad.pitch_change < 0 && this.shared_scratchpad.camera_extra_pitch > -1*max_change)
+			this.shared_scratchpad.camera_extra_pitch -= 1;
+		else
+		{
+			// bring back to center
+			if(this.shared_scratchpad.camera_extra_pitch > 0)
+				this.shared_scratchpad.camera_extra_pitch -= 1;
+			if(this.shared_scratchpad.camera_extra_pitch < 0)
+				this.shared_scratchpad.camera_extra_pitch += 1;
+		}
+		var pitch = new vec3(orientation[0][0], orientation[1][0], orientation[2][0]); // right
+		pitch = mult_vec_scalar(pitch, this.shared_scratchpad.pitch_change);
+		
+		var yaw = new vec3(orientation[0][1], orientation[1][1], orientation[2][1]); // up
+		yaw = mult_vec_scalar(yaw, this.shared_scratchpad.heading_change);
+
+		var roll = new vec3(-1*orientation[0][2], -1*orientation[1][2], -1*orientation[2][2]); //forward
+		var direction = roll;
+		roll = mult_vec_scalar(roll, this.shared_scratchpad.roll_change);
+		
+		var orientationChange = add(add(pitch, yaw), roll);
+		var angularChange = magnitude(orientationChange); // scalar
+		
+		var overallChange = mat4(1);
+		if(angularChange != 0) {
+			var rotationAxis = normalize(orientationChange); // vector
+			overallChange = rotation(angularChange, rotationAxis);
+		}
+		
+		var cameraRotation = mult(this.shared_scratchpad.orientation, overallChange);
+		this.shared_scratchpad.position = add(this.shared_scratchpad.position, mult_vec_scalar(normalize(direction), this.shared_scratchpad.speed));
+		
+		var transition = new mat4();
+		transition = mult(transition, translation(this.shared_scratchpad.position[0], this.shared_scratchpad.position[1], this.shared_scratchpad.position[2]));
+		transition = mult(transition, cameraRotation);
+		transition = mult(transition, translation(0,0,5));
+        graphics_state.camera_transform = inverse(transition);
+
 	}
 }, Animation);
